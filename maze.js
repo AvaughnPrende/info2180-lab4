ed497c916a9461865b5a9edadb1b88fab4096095
Avaughn Prendergast <AvaughnPrendergast@yahoo.com>
2017-10-07 06:42:36 -0500

    Task 2 Completed: All boundaries turn red with mouseover

--- maze.js
+++ maze.js
@@ -1,7 +1,24 @@
+/*
+
+Task 1
 
 window.onload = function (){
 	document.getElementById('boundary1').onmouseover = function(){
 		document.getElementById('boundary1').classList.add("youlose");
 	}
 }
+*/
+
+
+//Task 2
+
+window.onload = function(){
+	boundaries = document.querySelectorAll(".boundary");
+	for (i=0;i<boundaries.length;i++){
+		boundaries[i].addEventListener("mouseover", function(element){
+			this.classList.add("youlose");
+		});
+	};
+}
+
 
