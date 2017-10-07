8eeefe4c5c8326675566fb66f90b3d5bc36c68cb
Avaughn Prendergast <AvaughnPrendergast@yahoo.com>
2017-10-07 08:39:59 -0500

    Completed task 3: Alert box pops up when the game is won

--- maze.js
+++ maze.js
@@ -10,15 +10,40 @@ window.onload = function (){
 */
 
 
-//Task 2
+//Task 2-6s
+
+//Vriable Declarations
+beganMaze = false;
+endedMaze = false;
+touchedNoBoundaries = true;
 
 window.onload = function(){
+
+	start = document.getElementById("start");
+	end   = document.getElementById("end");
 	boundaries = document.querySelectorAll(".boundary");
+
+	//Boundary changes to red when a mouse touches it
 	for (i=0;i<boundaries.length;i++){
 		boundaries[i].addEventListener("mouseover", function(element){
 			this.classList.add("youlose");
+			touchedNoBoundaries = false;
 		});
 	};
+
+	//Alert Box pops up when game is won;
+	start.addEventListener("mouseover", function(){
+		beganMaze = true;
+	});
+
+	end.addEventListener("mouseover", function(){
+		endedMaze = true;
+
+		if (beganMaze && touchedNoBoundaries){
+			window.alert("YOU WIN");
+		}
+	});
 }
+
 
 
