94a4a6af588256c95109f629680a8309b728fe99
Avaughn Prendergast <AvaughnPrendergast@yahoo.com>
2017-10-19 21:08:47 -0500

    Restructured code and stopped example boundary from changing color

--- maze.js
+++ maze.js
@@ -13,51 +13,66 @@ window.onload = function (){
 //Task 2-6s
 
 //Vriable Declarations
-beganMaze = false;
-endedMaze = false;
+beganMaze           = false;
+endedMaze           = false;
 touchedNoBoundaries = true;
 
-window.onload = function(){
+window.onload  = function(){
 
-	start = document.getElementById("start");
-	end   = document.getElementById("end");
-	boundaries = document.querySelectorAll(".boundary");
-	maze = document.getElementById("maze");
+	start      = document.getElementById("start");
+	end        = document.getElementById("end");
+	boundaries = document.querySelectorAll('.boundary:not(.example)');
+	maze       = document.getElementById("maze");
 
 	//Boundary changes to red when a mouse touches it
 	for (i=0;i<boundaries.length;i++){
-		boundaries[i].addEventListener("mouseover", function(element){
-			
-		document.getElementById("status").innerHTML = "YOU LOSE";
+		boundaries[i].addEventListener("mouseover", turnWallsRed);
 
-			for (i=0;i<boundaries.length;i++){
+
+	function touchWallMessage(){
+		document.getElementById("status").innerHTML = "YOU LOSE: YOU TOUCHED THE WALL";
+	}
+
+	function turnWallsRed(){
+		touchWallMessage();
+		touchedNoBoundaries = false;
+
+		for (i=0;i<boundaries.length;i++){
 				boundaries[i].classList.add("youlose")
 			}
-			touchedNoBoundaries = false;
-		});
-	};
+		}
+	}
 
-
-	//Alert Box pops up when game is won;
-	start.addEventListener("mouseover", function(){
+	function changeBeganMazeValue(){
 		beganMaze = true;
-	});
+	}
 
-	start.addEventListener("click", function(){
-		location.reload();
-	})
-
-	end.addEventListener("mouseover", function(){
+	function changeEndedMazeValue(){
 		endedMaze = true;
-
 		if (beganMaze && touchedNoBoundaries){
 			document.getElementById("status").innerHTML = "YOU WIN";
-		} 
-	});
+		}
+	}
 
-	maze.onmouseleave = function(){
+	function locationReload(){
+		location.reload();
+	}
+
+	function exitMazeMessage(){
 		if (beganMaze === true){
 			document.getElementById("status").innerHTML = "YOU LOSE: YOU LEFT THE MAZE";
 		}
 	}
+
+	function removeListeners(){
+
+	}
+
+	start.addEventListener("mouseover", changeBeganMazeValue);
+
+	start.addEventListener("click", locationReload);
+
+	end.addEventListener("mouseover", changeEndedMazeValue);
+
+	maze.onmouseleave = exitMazeMessage;
 }
